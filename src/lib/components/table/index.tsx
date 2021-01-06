b03bd37654708b5fe1f2aebe0c0213a0f7416b74
Chris Kolkman <ckolkman@treorisoft.com>
2021-01-06 06:58:03 -0500

    add simple filter: SearchForm override - fixes #32

--- src/lib/components/table/index.tsx
+++ src/lib/components/table/index.tsx
@@ -168,6 +168,13 @@ export const DataTable = function<T>({paginate = 'both', quickEditPosition = 'bo
     ...props.qs
   });
 
+  const [rawFilter, setRawFilter] = useQueryState<{filter?: any}>({}, {
+    ...props.qs,
+    properties: {
+      filter: 'any'
+    }
+  });
+
   const [filter, setFilter] = useParsedQs<QueryFilterGroup, {filter?: any}>(
     { groupOperator: 'and', filters: [] },
     (qsFilter) => convertFromQS(qsFilter, columnData.actualColumns),
@@ -432,11 +439,19 @@ export const DataTable = function<T>({paginate = 'both', quickEditPosition = 'bo
       <div id={props.id} style={wrapperStyle} {...(props.tableContainerProps ?? {})} className={`ts-datatable ts-datatable-container ${props.tableContainerProps?.className ?? ''}`}>
         <div ref={topEl} className={`ts-datatable-top`}>
           <div className='ts-datatable-search-filters'>
+            {/* test3 */}
             {!hideSearchForm && <SearchForm
               searchQuery={searchQuery.query}
+              filter={rawFilter.filter}
               onSearch={(query) => {
                 batchedQSUpdate(() => {
                   setSearchQuery({ query });
+                  setPagination({ page: 1 });
+                });
+              }}
+              applyFilter={(newFilter) => {
+                batchedQSUpdate(() => {
+                  setRawFilter({ filter: newFilter });
                   setPagination({ page: 1 });
                 });
               }}
